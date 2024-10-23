9b60cd35d563cb6193de014a9e43f7128f0d2b51
connar <83016589+connor-john-x@users.noreply.github.com>
2024-10-23 15:36:29 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Page() {
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
-            , brought to you by Vercel.
+            , 有时间一起吃鸡.
           </p>
           <Link
             href="/login"
